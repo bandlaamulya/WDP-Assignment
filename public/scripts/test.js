511e9a9331895f392bad249d31d5cde9b5a4bb96
amulyabandla <amulya.bandla13@gmail.com>
2022-11-21 02:07:55 -0500

    new commit

--- public/scripts/test.js
+++ public/scripts/test.js
@@ -1,3 +1,37 @@
+document.getElementById("btn-users").addEventListener('click', getUsers);
+
+function getUsers() {
+  fetch("http://localhost:3000/users/")
+  .then((res)=> res.json())
+  .then((data) => console.log(data))
+  .catch((err)=> console.log(err))
+}
+
+
+
+async function fetchData(route = '', data = {}, GET) {
+    const response = await fetch(`http://localhost:3000${users}`, {
+      method: GET, // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    if(response.ok) {
+      return await response.json(); // parses JSON response into native JavaScript objects
+    } else {
+      throw await response.json();
+    }
+  }
+
+
+
+
 let detailDoc = document.getElementById("enterDetails");
 if(detailDoc) detailDoc.addEventListener("submit", customerObj);
 
