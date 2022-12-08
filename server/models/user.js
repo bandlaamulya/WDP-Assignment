21b8f70340b3644b2ee48af6844e0ef86334bc5b
bandlaamulya <amulya.bandla13@gmail.com>
2022-12-08 00:16:07 -0500

    user changes

--- server/models/user.js
+++ server/models/user.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // "database" as object literal
 const users = [
   {
@@ -25,9 +26,103 @@ function login(user) { // {email_id: "sda", password: "gsdhjsga"}
   let cUser = users.filter( u => u.email_id === user.email_id);
   
   if(!cUser[0]) throw Error("email_id not found");
+=======
+const con = require("./db_connect");
+
+// Table Creation 
+async function createTable() {
+  let sql=`CREATE TABLE IF NOT EXISTS users (
+    userID INT NOT NULL AUTO_INCREMENT,
+    firstname VARCHAR(255),
+    lastname VARCHAR(255),
+    email_id VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255),
+    CONSTRAINT userPK PRIMARY KEY(userID)
+  ); `
+  await con.query(sql);
+}
+createTable();
+
+// grabbing all users in database
+async function getAllUsers() {
+  const sql = `SELECT * FROM users;`;
+  let users = await con.query(sql);
+  console.log(users)
+}
+
+// Create  User - Registering
+async function register(user) {
+  let cUser = await getUser(user);
+  if(cUser.length > 0) throw Error("Username already in use");
+
+  const sql = `INSERT INTO users (firstname, lastname, email_id, password)
+    VALUES ("${user.firstname}", "${user.lastname}", "${user.email_id}", "${user.password}");
+  `
+  await con.query(sql);
+  return await login(user);
+}
+
+// Read User -- login user
+async function login(user) { // {userName: "sda", password: "gsdhjsga"}
+  let cUser = await getUser(user); //[{userName: "cathy123", password: "icecream"}]
+  
+  if(!cUser[0]) throw Error("Username not found");
+>>>>>>> 76aee62 (user changes)
   if(cUser[0].password !== user.password) throw Error("Password incorrect");
 
   return cUser[0];
 }
 
+<<<<<<< HEAD
 module.exports = {getAllUsers, login};
+=======
+// Update User function
+async function editUser(user) {
+  let sql = `UPDATE users 
+    SET email_id = "${user.email_id}"
+    WHERE userID = ${user.userID}
+  `;
+
+  await con.query(sql);
+  let updatedUser = await getUser(user);
+  return updatedUser[0];
+}
+
+// Delete User function
+async function deleteUser(user) {
+  let sql = `DELETE FROM users
+    WHERE userID = ${user.userID}
+  `
+  await con.query(sql);
+}
+
+// Useful Functions
+async function getUser(user) {
+  let sql;
+
+  if(user.userID) {
+    sql = `
+      SELECT * FROM users
+       WHERE userID = ${user.userID}
+    `
+  } else {
+    sql = `
+    SELECT * FROM users 
+      WHERE email_id = "${user.email_id}"
+  `;
+  }
+  return await con.query(sql);  
+}
+
+/*
+let cathy = {
+  userID: 5,
+  userName: "cathy123",
+  password: "icecream"
+}; 
+
+login(cathy);
+*/
+
+module.exports = { getAllUsers, login, register, editUser, deleteUser};
+>>>>>>> 76aee62 (user changes)
